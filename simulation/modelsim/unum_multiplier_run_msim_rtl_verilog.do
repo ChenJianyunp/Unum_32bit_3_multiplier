64315c9eae8f7c7ae78ed3542b3fa9e1f7402483
ChenJianyunp <1327079210@qq.com>
2017-11-13 01:31:27 +0100

    multiplier IP

--- simulation/modelsim/unum_multiplier_run_msim_rtl_verilog.do
+++ simulation/modelsim/unum_multiplier_run_msim_rtl_verilog.do
@@ -5,9 +5,10 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -vlog01compat -work work +incdir+F:/Unum/unumIII_32_3_multiplier_pipeline2 {F:/Unum/unumIII_32_3_multiplier_pipeline2/unum_multiplier.v}
+vlog -vlog01compat -work work +incdir+F:/Unum/unumIII_32_3_multiplier_pipeline3 {F:/Unum/unumIII_32_3_multiplier_pipeline3/unum_multiplier.v}
+vlog -vlog01compat -work work +incdir+F:/Unum/unumIII_32_3_multiplier_pipeline3 {F:/Unum/unumIII_32_3_multiplier_pipeline3/frac_mult.v}
 
-vlog -vlog01compat -work work +incdir+F:/Unum/unumIII_32_3_multiplier_pipeline2/simulation/modelsim {F:/Unum/unumIII_32_3_multiplier_pipeline2/simulation/modelsim/unum_multiplier.vt}
+vlog -vlog01compat -work work +incdir+F:/Unum/unumIII_32_3_multiplier_pipeline3/simulation/modelsim {F:/Unum/unumIII_32_3_multiplier_pipeline3/simulation/modelsim/unum_multiplier.vt}
 
 vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cycloneive_ver -L rtl_work -L work -voptargs="+acc"  unum_multiplier_vlg_tst
 
--- unum_multiplier.v
+++ unum_multiplier.v
@@ -21,6 +21,9 @@ reg[1:0] isInf_1;	 //isInf[1]: unum1    [0]: unum2    =1 if value is Inf
 reg[31:0] temp1,temp2;  //store changed or unchange input numbers
 reg[4:0] unum1_shift,unum2_shift; //result of zero/one counting
 wire[4:0] n1,n2;//result of leading zero count
+wire[30:0] unum1_2s,unum2_2s;
+assign unum1_2s=~unum1[30:0]+31'b1;
+assign unum2_2s=~unum2[30:0]+31'b1;
 always@(posedge clk)begin  ////////1st
 	if(unum1[30:0]==31'b0)begin isZero_1[1]<=unum1[31]; isInf_1[1]<=unum1[31]; end
 	else begin isZero_1[1]<=1'b1; isInf_1[1]<=1'b0;end 
@@ -28,9 +31,9 @@ always@(posedge clk)begin  ////////1st
 	if(unum2[30:0]==31'b0)begin isZero_1[0]<=unum2[31]; isInf_1[0]<=unum2[31]; end
 	else begin isZero_1[0]<=1'b1; isInf_1[0]<=1'b0; end
 	
-	if(unum1[31])begin temp1<=32'h8000_0000-unum1[31:0]; end  /// change unum from 2nd complement to original
+	if(unum1[31])begin temp1<=unum1_2s; end  /// change unum from 2nd complement to original
 	else begin temp1<=unum1[31:0]; end
-	if(unum2[31])begin temp2<=32'h8000_0000-unum2[31:0]; end
+	if(unum2[31])begin temp2<=unum2_2s; end
 	else begin temp2<=unum2[31:0]; end
 	
 	unum1_shift<=n1;
@@ -39,8 +42,8 @@ always@(posedge clk)begin  ////////1st
 	temp1[31]<=unum1[31];
 	temp2[31]<=unum2[31];
 end
-LZC lzc1(.x1({unum1[31:0],1'b1}),.n(n1));
-LZC lzc2(.x1({unum2[31:0],1'b1}),.n(n2));
+LZC lzc1(.x1(unum1_2s),.n(n1));
+LZC lzc2(.x1(unum2_2s),.n(n2));
 
 
 //2nd: Left shift the temp so that the exponent bits, sign bit and fraction bits will in the certain positions.
@@ -56,11 +59,11 @@ always@(posedge clk)begin        ///2nd
 	temp1_2[31]<=temp1[31];
 	temp2_2[31]<=temp2[31];	
 	
-	if(temp1[30]) begin expo_num1[8]<=1'b0; expo_num1[7:3]<=unum1_shift-5'b1; end
-	else begin expo_num1[8]<=1'b1; expo_num1[7:3]<=~unum1_shift+5'd1; end
+	if(temp1[30]) begin expo_num1[8]<=1'b0; expo_num1[7:3]<=unum1_shift; end
+	else begin expo_num1[8]<=1'b1; expo_num1[7:3]<=~unum1_shift; end
 	
-	if(temp2[30]) begin expo_num2[8]<=1'b0; expo_num2[7:3]<=unum2_shift-5'b1; end
-	else begin expo_num2[8]<=1'b1; expo_num2[7:3]<=~unum2_shift+5'd1; end
+	if(temp2[30]) begin expo_num2[8]<=1'b0; expo_num2[7:3]<=unum2_shift; end
+	else begin expo_num2[8]<=1'b1; expo_num2[7:3]<=~unum2_shift; end
 
 	isInf_2<=isInf_1[1]|isInf_1[0];
 	NaN_2<=(isInf_1[1]&~isZero_1[0])|(~isZero_1[1]&isInf_1[0]);
@@ -71,20 +74,40 @@ end
 ///3rd: multiple two fractions, add two exponent values
 reg isZero_3;
 reg isInf_3;  //if one of the input numbers is Inf, this bit will be 1
-reg[54:0] frac_numo_3; //[54]:sign bit   [53]:for carry on 			[52]:1.   	[51:0]fraction bits
+reg frac_numo_3; //[54]:sign bit   [53]:for carry on 			[52]:1.   	[51:0]fraction bits
 reg[1:0] expo_sign_3; //[1]: sign value of unum1     [0]: sign value of unum2
 reg[8:0] expo_numo_3; //store exponent values
 reg NaN_3;
+wire[53:25] mult_result;
 always@(posedge clk)begin         //3rd
-	frac_numo_3[53:0]<={isZero_2,temp1_2[26:1]}*{isZero_2,temp2_2[26:1]};
-	frac_numo_3[54]<=(temp1_2[31]^temp2_2[31])&isZero_2|isInf_2;
+	//frac_numo_3[53:0]<=*;
+	frac_numo_3<=(temp1_2[31]^temp2_2[31])&isZero_2|isInf_2;
 	NaN_3<=NaN_2;
 	expo_sign_3[1]<=expo_num1[8];
 	expo_sign_3[0]<=expo_num2[8];
-	expo_numo_3<={expo_num1,temp1_2[29:27]}+{expo_num2,temp2_2[29:27]};
+	expo_numo_3<={expo_num1,temp1_2[28:26]}+{expo_num2,temp2_2[28:26]};
 	isInf_3<=isInf_2;
 	isZero_3<=isZero_2;
 end
+
+frac_mult mult1(.clock(clk),.dataa({isZero_2,temp1_2[25:0]}),.datab({isZero_2,temp2_2[25:0]}) ,.result(mult_result[53:25]) );
+
+reg isZero_3_2;
+reg isInf_3_2;  //if one of the input numbers is Inf, this bit will be 1
+reg frac_numo_3_2; //[54]:sign bit   [53]:for carry on 			[52]:1.   	[51:0]fraction bits
+reg[1:0] expo_sign_3_2; //[1]: sign value of unum1     [0]: sign value of unum2
+reg[8:0] expo_numo_3_2; //store exponent values
+reg NaN_3_2;
+always@(posedge clk)begin         //3rd
+	isZero_3_2<=isZero_3;
+	isInf_3_2<=isInf_3;
+	frac_numo_3_2<=frac_numo_3;
+	expo_sign_3_2<=expo_sign_3;
+	expo_numo_3_2<=expo_numo_3;
+	NaN_3_2<=NaN_3;
+	
+end
+
 
 //4th: normalize the result of fraction multiplication 
 reg isZero_4;
@@ -93,12 +116,12 @@ reg[54:26] frac_numo_4; //[54]:sign bit    [53]:1.   [52:0]fraction bits
 reg[8:0] expo_numo_4; //store exponent values
 reg[1:0] expo_sign_4; //[1]: sign value of unum1     [0]: sign value of unum2
 always@(posedge clk)begin         //4th
-	frac_numo_4[54]<=frac_numo_3[54];
-	if(frac_numo_3[53])begin frac_numo_4[53:26]<=frac_numo_3[53:26];expo_numo_4<=expo_numo_3+9'd1;end
-	else begin  frac_numo_4[53:26]<=frac_numo_3[52:25]; expo_numo_4<=expo_numo_3; end
-	NaN_4<=NaN_3;
-	expo_sign_4<=expo_sign_3;
-	isZero_4<=isZero_3;
+	frac_numo_4[54]<=frac_numo_3_2;
+	if(mult_result[53])begin frac_numo_4[53:26]<=mult_result[53:26];expo_numo_4<=expo_numo_3_2+9'd1;end
+	else begin  frac_numo_4[53:26]<=mult_result[52:25]; expo_numo_4<=expo_numo_3_2; end
+	NaN_4<=NaN_3_2;
+	expo_sign_4<=expo_sign_3_2;
+	isZero_4<=isZero_3_2;
 end
 
 
@@ -134,25 +157,22 @@ endmodule
 
 //LZC(leading zero counter) module is designed based on MODULAR DESIGN OF FAST LEADING ZEROS COUNTING CIRCUIT (http://iris.elf.stuba.sk/JEEEC/data/pdf/6_115-05.pdf)
 module LZC(
-			input[32:0] x1,//[32:1] unum  [0]:1
+			input[30:0] x1,
 			output[4:0] n
 			);
 wire[7:0] a;
 wire[15:0] z;
 reg[31:0] x;
-reg[31:0] x2;
 reg [1:0] n1;
 wire[2:0] y;
 assign n[1:0]=n1[1:0];
 assign n[4:2]=y;
 
 always@(*)begin
-	if(x1[32])begin x2[31:1]={~x1[31:1]+32'b1}; end  //if number is negative,take 2's complement
-	else begin x2[31:1]=x1[31:1]; end
-	x2[0]=x1[0];
-	if(x2[31]) begin x[31:1]=~x2[31:1]; end			//if the number starts with 1, inverse it
-	else begin x[31:1]=x2[31:1]; end
-	x[0]=x2[0];
+
+	if(x1[30]) begin x[31:2]=~x1[29:0]; end			//if the number starts with 1, inverse it
+	else begin x[31:2]=x1[29:0]; end
+	x[1:0]=2'b10;
 	case(y)
 	3'b000: n1[1:0]=z[1:0];
 	3'b001: n1[1:0]=z[3:2];
